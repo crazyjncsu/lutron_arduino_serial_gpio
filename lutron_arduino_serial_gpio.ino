269ec82ee5f8b9884a3408f0f458bbcafc6f1070
jake <jmorgan@screenconnect.com>
2020-09-30 14:20:55 -0400

    fixed pin definitions

--- lutron_arduino_serial_gpio.ino
+++ lutron_arduino_serial_gpio.ino
@@ -2,10 +2,10 @@ const int pollIntervalMillisecondCount = 500;
 const int readTimeout = 2000;
 const int baudRate = 9600;
 
-const int inputPinCount = 4;
-const int outputPinCount = 4;
-const int processorAddressPinCount = 1; // number of values is "1 << pinCount" for these two, offset by the *Offset variables beloew
-const int keypadAddressPinCount = 1;
+const byte inputPins[] = { 13, 14, 15, 18, 19 };
+const byte outputPins[] = { 21, 22, 23, 25, 26 };
+const byte processorAddressPins[] = { 4 };
+const byte keypadAddressPins[] = { 5, 12 };
 
 const int inputButtonStart = 1; // 3rd column in 24 button keypad view
 const int outputButtonStart = 9; // 2nd column in 24 button keypad view
@@ -25,12 +25,6 @@ const int keypadLedStateLength = 41;
 const char keypadLedStateStart[] = "KLS";
 const int keypadLedStateButtonZeroIndex = 16;
 
-const int pinStart = 12;
-const int inputPinStart = pinStart;
-const int outputPinStart = inputPinStart + inputPinCount;
-const int processorAddressPinStart = outputPinStart + outputPinCount;
-const int keypadAddressPinStart = processorAddressPinStart + processorAddressPinCount;
-
 const int keypadLinkAddress = 6;
 
 const int bufferLength = 128;
@@ -42,26 +36,26 @@ int keypadAddress;
 
 void setup() {
   { // configure pins
-    for (auto i = 0; i < inputPinCount; i++)
-      pinMode(inputPinStart + i, INPUT_PULLUP);
+    for (auto i = 0; i < sizeof(inputPins); i++)
+      pinMode(inputPins[i], INPUT_PULLUP);
     
-    for (auto i = 0; i < processorAddressPinCount; i++)
-      pinMode(processorAddressPinStart + i, INPUT_PULLUP);
+    for (auto i = 0; i < sizeof(processorAddressPins); i++)
+      pinMode(processorAddressPins[i], INPUT_PULLUP);
     
-    for (auto i = 0; i < keypadAddressPinCount; i++)
-      pinMode(keypadAddressPinStart + i, INPUT_PULLUP);
+    for (auto i = 0; i < sizeof(keypadAddressPins); i++)
+      pinMode(keypadAddressPins[i], INPUT_PULLUP);
   
     pinMode(ledPin, OUTPUT);
   }
 
   { // calculate processor and keypad addresses from pins that are grounded
     processorAddress = processorAddressOffset;
-    for (auto i = 0; i < processorAddressPinCount; i++)
-      processorAddress += digitalRead(processorAddressPinStart + i) == LOW ? 1 << i : 0;
+    for (auto i = 0; i < sizeof(processorAddressPins); i++)
+      processorAddress += digitalRead(processorAddressPins[i]) == LOW ? 1 << i : 0;
    
     keypadAddress = keypadAddressOffset;
-    for (auto i = 0; i < keypadAddressPinCount; i++)
-      keypadAddress += digitalRead(keypadAddressPinStart + i) == LOW ? 1 << i : 0;
+    for (auto i = 0; i < sizeof(keypadAddressPins); i++)
+      keypadAddress += digitalRead(keypadAddressPins[i]) == LOW ? 1 << i : 0;
   }
 
   Serial.begin(baudRate);
@@ -77,15 +71,15 @@ void loop() {
 
   if (lineLength == keypadLedStateLength && strncmp(readBuffer, keypadLedStateStart, strlen(keypadLedStateStart)) == 0) {
     // send keypad button press for any input pin not matching keypad button led state
-    for (auto i = 0; i < inputPinCount; i++)
-      if ((digitalRead(inputPinStart + 1) == LOW) != (readBuffer[keypadLedStateButtonZeroIndex + inputButtonStart + i] == '1'))
+    for (auto i = 0; i < sizeof(inputPins); i++)
+      if ((digitalRead(inputPins[i]) == LOW) != (readBuffer[keypadLedStateButtonZeroIndex + inputButtonStart + i] == '1'))
         writeLineToSerial(writeBuffer, bufferLength, "KBP,[%d:%d:%d],%d", processorAddress, keypadLinkAddress, keypadAddress, inputButtonStart + i);
 
     // set output pin for every keypad led
-    for (auto i = 0; i < outputPinCount; i++) {
+    for (auto i = 0; i < sizeof(outputPins); i++) {
       auto isLedOn = readBuffer[keypadLedStateButtonZeroIndex + inputButtonStart + i] == '1';
-      pinMode(outputPinStart + 1, isLedOn ? OUTPUT : INPUT); // OUTPUT,LOW=grounded
-      digitalWrite(outputPinStart + i, isLedOn ? LOW : HIGH); // INPUT,HIGH=ungrounded
+      pinMode(outputPins[i], isLedOn ? OUTPUT : INPUT); // OUTPUT,LOW=grounded
+      digitalWrite(outputPins[i], isLedOn ? LOW : HIGH); // INPUT,HIGH=ungrounded
     }
   }
   
