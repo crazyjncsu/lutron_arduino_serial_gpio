610033e4c2df4aa21627632ec5edb4f7de04c96c
jake <jmorgan@screenconnect.com>
2020-10-01 11:21:39 -0400

    added loop for reading reshuffled some top stuff

--- lutron_arduino_serial_gpio.ino
+++ lutron_arduino_serial_gpio.ino
@@ -2,19 +2,19 @@ const int pollIntervalMillisecondCount = 500;
 const int readTimeout = 2000;
 const int baudRate = 9600;
 
+const int inputButtonStart = 1; // 3rd column in 24 button keypad view
+const int outputButtonStart = 9; // 2nd column in 24 button keypad view
+
+#ifdef ESP32
 const byte inputPins[] = { 13, 14, 15, 18, 19 };
 const byte outputPins[] = { 21, 22, 23, 25, 26 };
 const byte processorAddressPins[] = { 4 };
 const byte keypadAddressPins[] = { 5, 12 };
 
-const int inputButtonStart = 1; // 3rd column in 24 button keypad view
-const int outputButtonStart = 9; // 2nd column in 24 button keypad view
-
-// esp32 board I have is kind funny with this
-// no LED_BUILTIN was defined ... and the levels here
 const int ledPin = 27; // 27
 const int ledOnLevel = LOW; // cmon man
 const int ledOffLevel = HIGH;
+#endif
 
 // these are the base lutron IDs which we offset
 // so we don't have to read as many inputs
@@ -66,20 +66,22 @@ void loop() {
   digitalWrite(ledPin, ledOnLevel);
 
   writeLineToSerial(writeBuffer, bufferLength, "RKLS,[%d:%d:%d]", processorAddress, keypadLinkAddress, keypadAddress);
+
+  for (auto lineIndex = 0; lineIndex == 0 || Serial.available() > 0; lineIndex++) {
+    auto lineLength = readLineFromSerial(readBuffer, bufferLength);
+
+    if (lineLength == keypadLedStateLength && strncmp(readBuffer, keypadLedStateStart, strlen(keypadLedStateStart)) == 0) {
+      // send keypad button press for any input pin not matching keypad button led state
+      for (auto i = 0; i < sizeof(inputPins); i++)
+        if ((digitalRead(inputPins[i]) == LOW) != (readBuffer[keypadLedStateButtonZeroIndex + inputButtonStart + i] == '1'))
+          writeLineToSerial(writeBuffer, bufferLength, "KBP,[%d:%d:%d],%d", processorAddress, keypadLinkAddress, keypadAddress, inputButtonStart + i);
   
-  auto lineLength = readLineFromSerial(readBuffer, bufferLength);
-
-  if (lineLength == keypadLedStateLength && strncmp(readBuffer, keypadLedStateStart, strlen(keypadLedStateStart)) == 0) {
-    // send keypad button press for any input pin not matching keypad button led state
-    for (auto i = 0; i < sizeof(inputPins); i++)
-      if ((digitalRead(inputPins[i]) == LOW) != (readBuffer[keypadLedStateButtonZeroIndex + inputButtonStart + i] == '1'))
-        writeLineToSerial(writeBuffer, bufferLength, "KBP,[%d:%d:%d],%d", processorAddress, keypadLinkAddress, keypadAddress, inputButtonStart + i);
-
-    // set output pin for every keypad led
-    for (auto i = 0; i < sizeof(outputPins); i++) {
-      auto isLedOn = readBuffer[keypadLedStateButtonZeroIndex + inputButtonStart + i] == '1';
-      pinMode(outputPins[i], isLedOn ? OUTPUT : INPUT); // OUTPUT,LOW=grounded
-      digitalWrite(outputPins[i], isLedOn ? LOW : HIGH); // INPUT,HIGH=ungrounded
+      // set output pin for every keypad led
+      for (auto i = 0; i < sizeof(outputPins); i++) {
+        auto isLedOn = readBuffer[keypadLedStateButtonZeroIndex + inputButtonStart + i] == '1';
+        pinMode(outputPins[i], isLedOn ? OUTPUT : INPUT); // OUTPUT,LOW=grounded
+        digitalWrite(outputPins[i], isLedOn ? LOW : HIGH); // INPUT,HIGH=ungrounded
+      }
     }
   }
   
